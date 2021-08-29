8ad8d3031b2fd2cb5a6d9fed13b0f8b1788fbf54
Brentt Smith <brentt.smith@gfs.com>
2021-08-29 14:07:17 -0400

    changed from if/else to switch

--- src/main/java/com/example/mazerunner/parts/CardinalDirection.java
+++ src/main/java/com/example/mazerunner/parts/CardinalDirection.java
@@ -1,5 +1,8 @@
 package com.example.mazerunner.parts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum CardinalDirection {
 
     EAST("E"),
@@ -10,6 +13,13 @@ public enum CardinalDirection {
     ;
 
     private final String direction;
+    private static final Map<String, CardinalDirection> lookup = new HashMap<>();
+
+    static {
+        for (final CardinalDirection direction : CardinalDirection.values()) {
+            lookup.put(direction.getDirection(), direction);
+        }
+    }
 
     CardinalDirection(final String direction) {
         this.direction = direction;
@@ -17,5 +27,9 @@ public enum CardinalDirection {
 
     public String getDirection() {
         return direction;
+    }
+
+    public static CardinalDirection getByName(final String name) {
+        return lookup.get(name);
     }
 }
