9f1f6f9b12125611ec305f8ce6fb9bc73fe356d9
Brentt Smith <brentt.smith@gfs.com>
2021-08-31 15:06:41 -0400

    benchmark changes

--- src/main/java/com/example/mazerunner/benchmark/MazeBenchmarker.java
+++ src/main/java/com/example/mazerunner/benchmark/MazeBenchmarker.java
@@ -11,8 +11,13 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 
+import com.example.mazerunner.exceptions.FoundExitException;
+import com.example.mazerunner.exceptions.MazeException;
+import com.example.mazerunner.exceptions.WallException;
+import com.example.mazerunner.navigation.steppers.CardinalStepper;
+import com.example.mazerunner.parts.CardinalDirection;
 import com.example.mazerunner.parts.Coordinates;
-import com.example.mazerunner.parts.FoundExitException;
+import com.example.mazerunner.parts.Maze;
 import com.example.mazerunner.parts.MazeMaster;
 
 public class MazeBenchmarker {
@@ -23,16 +28,24 @@ public class MazeBenchmarker {
     public void benchmark_test() throws IOException, URISyntaxException {
 
         final MazeMaster mazeMaster = new MazeMaster("theEndlessLoop.txt");
+        final Maze maze = mazeMaster.getMaze();
+        final CardinalStepper cardinalStepper = new CardinalStepper();
         final Coordinates coordinates = new Coordinates();
-        final List<String> directions = Arrays.asList("E", "E", "E", "E", "S", "S", "S", "S", "W", "W", "W", "W", "N", "N", "N", "N");
-        for (int i = 0; i < 100000; i++) {
+        final List<String> directions = Arrays.asList("E", "E", "E", "E", "E", "S", "S", "S", "S", "W", "W", "W", "W", "W", "N", "N", "N", "N");
 
-            for (final String direction : directions) {
-                try {
-                    mazeMaster.step(direction, coordinates);
-                } catch (final FoundExitException e) {
-                    //                System.out.println(e.getMessage());
+        for (int i = 0; i < 100000; i++) {
+            try {
+                for (final String direction : directions) {
+                    final CardinalDirection stepDirection = CardinalDirection.getByName(direction);
+                    cardinalStepper.doStep(maze, stepDirection, coordinates);
                 }
+
+            } catch (final FoundExitException e) {
+                System.out.println("Found the exit after " + i + " steps.");
+            } catch (final WallException we) {
+                System.out.println("Hit a wall after " + i + " steps.");
+            } catch (final MazeException me) {
+                throw new IllegalArgumentException(me.getMessage());
             }
         }
     }
