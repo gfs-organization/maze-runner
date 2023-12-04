f45640e476a9b29750a935fc66bf143290f190c0
Brentt Smith <94175207+brentt-smith@users.noreply.github.com>
2023-12-04 13:38:47 -0500

    flip-to-gradle: changed over to gradle and upgraded to Spring 3

--- src/main/java/com/example/mazerunner/Application.java
+++ src/main/java/com/example/mazerunner/Application.java
@@ -1,70 +1,67 @@
 package com.example.mazerunner;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeoutException;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import static com.example.mazerunner.config.Constants.SPLIT_API_KEY;
+import static com.example.mazerunner.config.Constants.SPLIT_PROD_DOMAIN;
+import static com.example.mazerunner.config.Constants.SPLIT_STAGE_DOMAIN;
 
 import com.example.mazerunner.parts.MazeMaster;
 import io.split.client.SplitClient;
 import io.split.client.SplitClientConfig;
 import io.split.client.SplitFactory;
 import io.split.client.SplitFactoryBuilder;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    private static final String SPIT_API_KEY = "8qm4hdpqnufit0rhruu1ar8a24b6jcvr7jdb"; // staging
-    //private static final String SPIT_API_KEY = "hqcsjesdq8iuk56kps9f28hlr2v2smvikdgv"; // personal
 
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+  @Bean
+  public Map<Integer, MazeMaster> mazeMasterList() throws IOException {
+    final Map<Integer, MazeMaster> mazeMasters = new HashMap<>();
+
+    mazeMasters.put(1, new MazeMaster("theSnake.txt"));
+    mazeMasters.put(2, new MazeMaster("theTrapper.txt"));
+    mazeMasters.put(3, new MazeMaster("theDrain.txt"));
+    mazeMasters.put(4, new MazeMaster("theCurl.txt"));
+    mazeMasters.put(5, new MazeMaster("theDoubleBack.txt"));
+    mazeMasters.put(6, new MazeMaster("theQuadrupleBypass.txt"));
+    mazeMasters.put(7, new MazeMaster("theSpellingBee.txt"));
+    mazeMasters.put(8, new MazeMaster("gregsMaze.txt"));
+
+    mazeMasters.put(9, new MazeMaster("firstFloor.txt", "secondFloor.txt", "thirdFloor.txt"));
+
+    return mazeMasters;
+  }
+
+  @Bean
+  public SplitClient splitClient() throws IOException, URISyntaxException, InterruptedException {
+    final SplitClientConfig config = SplitClientConfig.builder()
+        .endpoint("https://sdk." + SPLIT_STAGE_DOMAIN, "https://events." + SPLIT_PROD_DOMAIN)
+        .authServiceURL("https://auth." + SPLIT_STAGE_DOMAIN + "/api/auth")
+        .telemetryURL("https://telemetry." + SPLIT_STAGE_DOMAIN + "/api/v1")
+        .setBlockUntilReadyTimeout(10000)
+        .build();
+
+    final SplitFactory splitFactory = SplitFactoryBuilder.build(SPLIT_API_KEY, config);
+    final SplitClient client = splitFactory.client();
+    try {
+      client.blockUntilReady();
+    } catch (final TimeoutException e) {
+      // log & handle
     }
 
-    @Bean
-    public Map<Integer, MazeMaster> mazeMasterList() throws IOException {
-        final Map<Integer, MazeMaster> mazeMasters = new HashMap<>();
-
-        mazeMasters.put(1, new MazeMaster("theSnake.txt"));
-        mazeMasters.put(2, new MazeMaster("theTrapper.txt"));
-        mazeMasters.put(3, new MazeMaster("theDrain.txt"));
-        mazeMasters.put(4, new MazeMaster("theCurl.txt"));
-        mazeMasters.put(5, new MazeMaster("theDoubleBack.txt"));
-        mazeMasters.put(6, new MazeMaster("theQuadrupleBypass.txt"));
-        mazeMasters.put(7, new MazeMaster("theSpellingBee.txt"));
-        mazeMasters.put(8, new MazeMaster("gregsMaze.txt"));
-
-        mazeMasters.put(9, new MazeMaster("firstFloor.txt", "secondFloor.txt", "thirdFloor.txt"));
-
-        return mazeMasters;
-    }
-
-    @Bean
-    public SplitClient splitClient() throws IOException, URISyntaxException {
-        //@formatter:off
-        final SplitClientConfig config = SplitClientConfig.builder()
-                .endpoint("https://sdk.split-stage.io","https://events.split-stage.io")
-                .authServiceURL("https://auth.split-stage.io/api/auth")
-                .telemetryURL("https://telemetry.split-stage.io/api/v1")
-                .setBlockUntilReadyTimeout(10000)
-                .build();
-        //@formatter:on
-
-        final SplitFactory splitFactory = SplitFactoryBuilder.build(SPIT_API_KEY, config);
-        final SplitClient client = splitFactory.client();
-        try {
-            client.blockUntilReady();
-        } catch (final TimeoutException | InterruptedException e) {
-            // log & handle
-        }
-
-        return client;
-    }
+    return client;
+  }
 
 }
--- src/test/java/com/example/mazerunner/ApplicationTest.java
+++ src/test/java/com/example/mazerunner/ApplicationTest.java
@@ -1,14 +1,15 @@
 package com.example.mazerunner;
 
-import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Application.class)
 public class ApplicationTest {
 
-    @Test
-    public void shouldFindMyWayThroughMaze1() throws Exception {
-        Assert.assertTrue(true);
-    }
+  @Test
+  public void shouldFindMyWayThroughMaze1() throws Exception {
+    assertTrue(true);
+  }
 }
